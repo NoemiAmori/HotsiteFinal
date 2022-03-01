15b52fd7b52193089476fd4a2cdef67a81745b86
NoemiAmori <noemiamorim910@gmail.com>
2022-03-01 13:35:37 -0300

    Atualização Final

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Hotsite.Models;
+using MySql.Data.MySqlClient;
+
+namespace Hotsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Interesse cad)
+        {
+            try
+            {
+                DatabaseService dbs = new DatabaseService();
+                dbs.CadastraInteresse(cad);
+                return Json (new {status = "OK"});
+            }
+    
+            catch (Exception e)
+            {
+                _logger.LogError("Falha na leitura: " + e.Message);
+                return Json (new {status= "Erro", mensagem="Erro ao gravar no BD"});
+            }
+        }
+    }
+}
+    
---
+++ wwwroot/js/site.js
@@ -0,0 +1,43 @@
+﻿function Cadastrar()
+{
+   let paramentros =
+   {
+       Nome: $("#nome").val(),
+       Email: $("#email").val(),
+       Mensagem: $("#mensagem").val()
+   }
+
+   $.post('/Home/Cadastrar', paramentros)
+   .done(
+     function(data){
+         if(data.status=="OK"){
+             $("#frm").after("<h3>Dados cadastrados com sucesso</h3>");
+             $("#frm").hide();
+             
+         }
+         else{
+            $("#frm").after("<h3>"+data.mensagem+"</h3>");
+         }
+     }
+   )
+
+   .fail(
+       function(){
+           alert("Ocorreu um erro de Conexão!!!")
+       }
+   );
+
+}
+$(document).ready(
+    function()
+    {
+        $("#frmCadastro").submit(
+            function(e)
+            {
+                e.preventDefault();
+                Cadastrar();
+            }
+        )
+    }
+);
+
